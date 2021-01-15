5856b31034c6103fceaada934e20461a8781a0ed
Ernest Muroiwa <emuroiwa@gmail.com>
2021-01-15 08:05:37 +0200

    first commit

---
+++ variables.py
@@ -0,0 +1,17 @@
+# Variables store data in memory
+
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (firstname, Firstname, FirstName and FIRSTNAME are different variables)
+
+# INVALID variables 
+# first-name
+# num-1
+# 1num
+
+# VALID variables 
+
+name = 'Ernest'
+age = '20'
+is_tall = True
